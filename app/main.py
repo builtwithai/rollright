41f0f2e9939b1dadbbd65705a91239e7655c2f3f
sminnie <minnie@sankhe.com>
2023-11-26 13:09:45 -0700

    Added function to predict video

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 import numpy as np
+import cv2
 import tensorflow as tf
 from PIL import Image
+import os
+import glob
 
 # Path to the TensorFlow Lite model file
 tflite_model_path = 'models/model_unquant.tflite'
@@ -13,56 +16,91 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
+# Load and preprocess image
 def load_and_preprocess_image(image_path):
-    # Load the image
-    image = Image.open(image_path).convert('RGB')
-
-    # Resize the image to match the input size of the model
+    print(f"image_path: {image_path}")
+    image = Image.open(image_path)
     image = image.resize((input_details[0]['shape'][2], input_details[0]['shape'][1]))
-
-    # Convert to numpy array and normalize
     image_array = np.array(image, dtype=np.float32) / 255.0
-
-    # Add a batch dimension
     image_array = np.expand_dims(image_array, axis=0)
     return image_array
 
+# Create labels
 def create_labels():
     labels = np.array(["Door", "Not Door"])
     return labels
 
 labels = create_labels()
 
-def predict(image_path):
-    # Preprocess the image
-    preprocessed_image = load_and_preprocess_image(image_path)
 
-    # Set the tensor to point to the input data to be inferred
-    interpreter.set_tensor(input_details[0]['index'], preprocessed_image)
+def predict(image):
+    """
+    Predicts the label for an image.
 
-    # Run the inference
+    Args:
+        image (numpy.ndarray): The image array to be predicted.
+
+    Returns:
+        numpy.ndarray: The predicted label for the image.
+    """
+    interpreter.set_tensor(input_details[0]['index'], image)
     interpreter.invoke()
-
-    # Extract the output
     output_data = interpreter.get_tensor(output_details[0]['index'])
     return output_data
 
-# Example usage
-import os
-import glob
+
+# Predict label for an image file
 
 def predict_label(image_path):
-    result = predict(image_path)
+    """
+    Predicts the label for an image.
+
+    Args:
+        image_path (str): The path to the image file.
+
+    Returns:
+        str: The predicted label for the image.
+    """
+    image = load_and_preprocess_image(image_path)
+    result = predict(image)
     predicted_class_index = np.argmax(result)
     predicted_class_label = labels[predicted_class_index]
-    print(f"Predicted label for {image_path}: {predicted_class_label}")
     return predicted_class_label
 
-# Specify the directory
-directory = 'data/'
 
-# Get a list of all image paths in the directory
-image_paths = glob.glob(os.path.join(directory, '*.jpeg'))
+# Preprocess image for model input
+def preprocess(image):
+    if len(input_details[0]['shape']) < 3:
+        print("Error: The model's input shape is invalid.")
+        return
+    height = input_details[0]['shape'][1]
+    width = input_details[0]['shape'][2]
+    if height <= 0 or width <= 0:
+        print("Error: The model's input height and width are invalid.")
+        return
+    image = cv2.resize(image, (width, height))
+    image_array = np.array(image, dtype=np.float32) / 255.0
+    return image_array
 
-for image_path in image_paths:
-    predict_label(image_path)
+# Predict label for each frame in a video
+def predict_label_from_video(video_path):
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        print("Error opening video")
+        return
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret:
+            processed_frame = preprocess(frame)
+            input = np.expand_dims(processed_frame, axis=0)
+            prediction = predict(input)
+            index = np.argmax(prediction)
+            predicted_class_label = labels[index]
+            # print(f"Predicted label for frame: {predicted_class_label}")
+        else:
+            break
+    cap.release()
+    cv2.destroyAllWindows
+    return predicted_class_label
+
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,12 @@
 import unittest
 import sys
+import cv2
+import sys
+
 sys.path.append('app')
 
-from main import predict_label  # replace with your actual module
+from main import predict_label  
+from main import predict_label_from_video
 
 class TestPredictLabel(unittest.TestCase):
     def test_predict_label_door(self):
@@ -11,7 +15,7 @@ class TestPredictLabel(unittest.TestCase):
 
         # Call the function with the test image
         result = predict_label(image_path)
-        print("result: ", result)
+        print(f"Predicted label for {image_path}: {result}")
 
         # Check that the result is as expected
         # This will depend on what your function returns
@@ -24,11 +28,25 @@ class TestPredictLabel(unittest.TestCase):
 
         # Call the function with the test image
         result = predict_label(image_path)
+        print(f"Predicted label for {image_path}: {result}")
 
         # Check that the result is as expected
         # This will depend on what your function returns
         # For example, if it returns a label, you might check that the label is correct
         self.assertEqual(result, 'Not Door')
+    
+    def test_predict_label_from_video(self):
+        # Use a known image for the test
+        video_file_path = 'tests/test.mp4'
+        result=predict_label_from_video(video_file_path)
+        print(f"Predicted label for {video_file_path}: {result}")
+
+        # Check that the result is as expected
+        # This will depend on what your function returns
+        # For example, if it returns a label, you might check that the label is correct
+        self.assertEqual(result, 'Door')
+
+
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main()       
